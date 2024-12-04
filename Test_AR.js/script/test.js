2a1e30741fb81a58bd08c5b70bece67992f759dd
timehddud01 <162527949+timehddud01@users.noreply.github.com>
2024-12-04 15:33:11 +0900

    tst3

--- Test_AR.js/script/test.js
+++ Test_AR.js/script/test.js
@@ -2,58 +2,34 @@ let rot = 0;
 let rotSpeed = 0.1;
 
 function setup() {
-  // AR.js와 연동된 p5 캔버스를 생성
-  createARCanvas();
+  // 일반 캔버스를 생성
+  createCanvas(400, 400, WEBGL); // WebGL을 사용해야 3D 객체가 렌더링됨
 }
 
 function draw() {
-  // 배경을 투명하게 유지하여 AR 카메라와 연동
-  clear();
+  background(0, 0, 0, 0); // 배경을 투명하게 설정
 
   // 회전 박스(녹색)
   push();
   rotateY(rot);
   fill(100, 240, 100);
-  box(20);
+  box(50);
   pop();
 
   // 왼쪽의 빨간 박스
   push();
-  translate(-30, 0, 0);
+  translate(-80, 0, 0);
   fill(240, 100, 100);
-  box(20);
+  box(50);
   pop();
 
   // 오른쪽의 파란 박스
   push();
-  translate(30, 0, 0);
+  translate(80, 0, 0);
   fill(100, 100, 240);
-  box(20);
+  box(50);
   pop();
 
   // 회전 속도 업데이트
   rot += rotSpeed;
 }
-
-// function setup() {
-//   createARCanvas();
-// }
-
-// let rot = 0;
-// let rotSpeed = 0.1;
-
-// function draw() {
-//   rot += rotSpeed;
-//   rotateY(rot);
-//   fill(100, 240, 100);
-
-//   box(20);
-
-//   translate(-20, 0, 0);
-//   fill(240, 100, 100);
-//   box(20);
-
-//   translate(40, 0, 0);
-//   fill(100, 100, 240);
-//   box(20);
-// }
