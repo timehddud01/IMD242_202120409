c55a25fd6455c75bf4bd549f8249ca6eea085ad5
timehddud01 <162527949+timehddud01@users.noreply.github.com>
2024-12-04 15:38:58 +0900

    tst3

--- Test_AR.js/script/test.js
+++ Test_AR.js/script/test.js
@@ -2,28 +2,28 @@ let rot = 0;
 let rotSpeed = 0.1;
 
 function setup() {
-  // 일반 캔버스를 생성
-  createCanvas(400, 400, WEBGL); // WebGL을 사용해야 3D 객체가 렌더링됨
+  // p5.js 캔버스 생성
+  createCanvas(windowWidth, windowHeight, WEBGL);
 }
 
 function draw() {
-  background(0, 0, 0, 0); // 배경을 투명하게 설정
+  background(0); // 검정색 배경
 
-  // 회전 박스(녹색)
+  // 회전하는 녹색 박스
   push();
   rotateY(rot);
   fill(100, 240, 100);
   box(50);
   pop();
 
-  // 왼쪽의 빨간 박스
+  // 왼쪽에 빨간 박스
   push();
   translate(-80, 0, 0);
   fill(240, 100, 100);
   box(50);
   pop();
 
-  // 오른쪽의 파란 박스
+  // 오른쪽에 파란 박스
   push();
   translate(80, 0, 0);
   fill(100, 100, 240);
